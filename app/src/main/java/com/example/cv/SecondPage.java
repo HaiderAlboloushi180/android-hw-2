a2dc7a9eabc3cb9be70f4fb484ea767516fbcc8c
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-03-13 01:52:06 +0300

    almost done with second bonus

--- app/src/main/java/com/example/cv/SecondPage.java
+++ app/src/main/java/com/example/cv/SecondPage.java
@@ -2,7 +2,11 @@ package com.example.cv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondPage extends AppCompatActivity {
@@ -17,6 +21,8 @@ public class SecondPage extends AppCompatActivity {
         TextView jobText   = findViewById(R.id.job);
         TextView phoneText = findViewById(R.id.phone);
         TextView emailText = findViewById(R.id.email);
+        Button email = findViewById(R.id.emailButton);
+        Button call = findViewById(R.id.callButton);
         Bundle claim = getIntent().getExtras();
 
         String nameClaimed  = claim.getString("name");
@@ -30,6 +36,25 @@ public class SecondPage extends AppCompatActivity {
         jobText.setText(jobClaimed);
         phoneText.setText(phoneClaimed);
         emailText.setText(emailClaimed);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + emailClaimed));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "body");
+
+                startActivity(Intent.createChooser(emailIntent, "Chooser Title"));            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneClaimed));
+                startActivity(intent);
+            }
+        });
 
     }
 }
